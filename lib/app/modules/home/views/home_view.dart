e32ec3a75ce07e45c66d0fd0f8a47ca1441850ef
virgi-septian <virgiseptian77@gmail.com>
2023-02-23 09:27:22 +0700

    membuat halaman home view

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-
+import 'package:lottie/lottie.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:google_fonts/google_fonts.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
@@ -9,14 +11,33 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: HexColor('#feeee8'),
       appBar: AppBar(
         title: const Text('HomeView'),
         centerTitle: true,
       ),
       body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Lottie.network(
+              'https://gist.githubusercontent.com/olipiskandar/2095343e6b34255dcfb042166c4a3283/raw/d76e1121a2124640481edcf6e7712130304d6236/praujikom_kucing.json',
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Text(
+              'Aplikasi Portal Berita Irlandia Utara',
+              style: GoogleFonts.roboto(
+                textStyle: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
