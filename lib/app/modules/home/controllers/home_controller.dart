8a74955e959826dd4474b3c6346a5d5b0925e472
virgi-septian <virgiseptian77@gmail.com>
2023-02-23 13:35:11 +0700

    memulai halaman dashboard tanpa login kembali

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,21 +1,25 @@
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:project_ujikom/app/modules/dashboard/bindings/dashboard_binding.dart';
+import 'package:project_ujikom/app/modules/dashboard/views/dashboard_view.dart';
 import 'dart:async';
 import 'package:project_ujikom/app/modules/login/views/login_view.dart';
 
 class HomeController extends GetxController {
   //TODO: Implement HomeController
   late Timer _pindah;
+  final authToken = GetStorage();
   // final count = 0.obs;
   @override
   void onInit() {
     _pindah = Timer.periodic(
-      const Duration(seconds: 4),
-      (timer) => Get.off(
-        () => const LoginView(),
-        transition: Transition.leftToRight,
-      ),
-    );
-    super.onInit();
+        const Duration(seconds: 4),
+        (timer) => authToken.read('token') == null
+            ? Get.off(
+                () => const LoginView(),
+                transition: Transition.leftToRight,
+              )
+            : Get.off(() => const DashboardView()));
   }
 
   @override
