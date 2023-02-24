941329a5594c45ac792fe9cf37f454f7cf1e4f82
virgi-septian <virgiseptian77@gmail.com>
2023-02-24 13:29:52 +0700

    menyiapkan fungsi untuk memanggil API

--- lib/app/modules/dashboard/controllers/dashboard_controller.dart
+++ lib/app/modules/dashboard/controllers/dashboard_controller.dart
@@ -1,7 +1,36 @@
+import 'dart:convert';
+
 import 'package:get/get.dart';
+import 'package:project_ujikom/app/data/entertainment_response.dart';
+import 'package:project_ujikom/app/data/headline_response.dart';
+import 'package:project_ujikom/app/data/sports_response.dart';
+import 'package:project_ujikom/app/data/technology_response.dart';
+
+import '../../../utils/api.dart';
 
 class DashboardController extends GetxController {
   //TODO: Implement DashboardController
+  final _getConnect = GetConnect();
+
+  Future<HeadlineResponse> getHeadline() async {
+    final response = await _getConnect.get(BaseUrl.headline);
+    return HeadlineResponse.fromJson(jsonDecode(response.body));
+  }
+
+  Future<EntertainmentRespone> getEntertainment() async {
+    final response = await _getConnect.get(BaseUrl.entertainment);
+    return EntertainmentRespone.fromJson(jsonDecode(response.body));
+  }
+
+  Future<SportsRespone> getSports() async {
+    final response = await _getConnect.get(BaseUrl.sports);
+    return SportsRespone.fromJson(jsonDecode(response.body));
+  }
+
+  Future<TechnologyRespone> getTechnology() async {
+    final response = await _getConnect.get(BaseUrl.technology);
+    return TechnologyRespone.fromJson(jsonDecode(response.body));
+  }
 
   final count = 0.obs;
   @override
